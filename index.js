cc511343f249801b46b6bb886c14e4e9c94d1143
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-10-12 16:50:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function moveDodgerRight() {
     let rightNumbers = dodger.style.right.replace("px", "");
     let right = parseInt(rightNumbers, 10);
 
-    if (left > 0) {
+    if (right > 0) {
       dodger.style.left = `${left + 1}px`;
     }
   }
